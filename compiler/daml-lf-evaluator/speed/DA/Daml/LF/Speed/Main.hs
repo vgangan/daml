eb4d3ca1043405d4d24fbb4cb3598d1846dfc771
Nick Chapman <nick.chapman@digitalasset.com>
2020-04-07 08:14:28 +0100

    haskell nfib speed test

---
+++ compiler/daml-lf-evaluator/speed/DA/Daml/LF/Speed/Main.hs
@@ -0,0 +1,40 @@
+-- Copyright (c) 2019 The DAML Authors. All rights reserved.
+-- SPDX-License-Identifier: Apache-2.0
+
+module DA.Daml.LF.Speed.Main
+  ( main
+  ) where
+
+import Data.Time (getCurrentTime,diffUTCTime)
+
+main :: IO ()
+main = do
+  putStrLn "speed nfib test"
+  mapM_ runTest args
+    where args = [30..40]
+          runTest arg = do stats <- timedNfib arg; print stats
+
+data Stats = Stats
+  { func :: String
+  , arg :: Int
+  , res :: Int
+  , duration :: Seconds
+  , speed :: Speed } deriving Show
+
+type Seconds = Double
+type Speed = Double
+
+timedNfib :: Int -> IO Stats
+timedNfib arg = do
+  let func = "nfib"
+  before <- getCurrentTime
+  let !res = nfib arg
+  after <- getCurrentTime
+  let duration = realToFrac $ diffUTCTime after before
+  let speed :: Speed = fromIntegral res / duration
+  return $ Stats { func, arg, res, duration, speed }
+
+nfib :: Int -> Int
+nfib 0 = 0
+nfib 1 = 0
+nfib n = nfib (n-1) + nfib (n-2) + 1
